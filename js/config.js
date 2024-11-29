57004e6c4154ef3e95942c52a501f06c092deb36
HK200202-hub <hari200202@gmail.com>
2024-11-28 19:50:01 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_Fp6P0LFWJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1gktv2fls6mqq2msa2c4d25n5t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1rm22323bs0dsspmrfmmitkujq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
