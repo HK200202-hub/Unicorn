f240d9438fcb220dedd449e71ba2f1e1f55f31b1
HK200202-hub <hari200202@gmail.com>
2024-12-10 11:17:12 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_N57LhmYyD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4rcufadi3jmukd3e4c73tg88uo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
